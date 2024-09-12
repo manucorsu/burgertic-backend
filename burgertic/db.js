5f0a306c1faa8fc25c952f624491bce21ec91f82
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-09-12 10:08:29 -0300

    update config para nuevo .env

--- burgertic/db.js
+++ burgertic/db.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
 
 export const config = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
     port: 5432,
     ssl: true,
 };
